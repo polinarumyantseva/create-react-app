f43ab9988ef3ea53c8175e0c33abf12a63bccd14
Polina Rumyantseva <polinrumyantseva@gmail.com>
2024-11-01 19:01:12 -0700

    Creating an element without JSX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
+import { createElement } from 'react';
 import './App.css';
 
-// декларативный
 export const App = () => {
-	return (
-		<div className="App">
-			{/* декларативный */}
-			<header className="App-header">
-				{/* декларативный */}
-				<img src={logo} className="App-logo" alt="logo" />
-				{/* декларативный */}
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				{/* декларативный */}
-				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-					Learn React
-				</a>
-				{/* декларативный */}
-				<p>{new Date().getFullYear()}</p>
-			</header>
-		</div>
+	return createElement(
+		'div',
+		{ className: 'App' },
+		createElement(
+			'div',
+			{ className: 'App-header' },
+			createElement('img', { className: 'App-logo', src: logo, alt: 'logo' }),
+			createElement('p', null, 'Edit ', createElement('code', null, 'src/App.js'), ' and save to reload.'),
+			createElement(
+				'a',
+				{ className: 'App-link', href: 'https://reactjs.org', target: '_blank', rel: 'noopener noreferrer' },
+				'Learn React',
+			),
+			createElement('p', null, new Date().getFullYear()),
+		),
 	);
 };
